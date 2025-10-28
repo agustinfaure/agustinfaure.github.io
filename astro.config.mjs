da86a853e6248cbf27315f4e0d1fe442c7e7ab0c
Agustin Faure <agustin.faure@icloud.com>
2025-10-28 09:49:27 +0100

    ci: deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import tailwind from '@astrojs/tailwind';
+
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://agustinfaure.github.io",
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx()]
+  integrations: [mdx(), tailwind()]
 });
