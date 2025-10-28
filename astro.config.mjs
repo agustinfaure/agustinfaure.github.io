5ab97ffaf08e189b0133e0f773b0ed7d6ab35b3d
Agustin Faure <agustin.faure@icloud.com>
2025-10-28 10:50:35 +0100

    working picture

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import tailwind from '@astrojs/tailwind';
-
+import tailwind from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://agustinfaure.github.io",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwind()]
   },
 
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx()]
 });
